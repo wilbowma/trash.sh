22f8da4886ff827541760c01356356dfba6a62b3
William J Bowman <bluephoenix47@gmail.com>
2012-05-30 19:25:15 -0400

    Got most the stubs filled in, though they aren't quite right yet.

--- trash.sh
+++ trash.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# TODO: figure out copyright
+# TODO: add verbose function
+# TODO: Check that all strings are $IFS safe.
+# Copyright (C) 2012 William J. Bowman <wjb@williamjbowman.com>
 
 usage()
 {
@@ -28,8 +32,12 @@ Trash (not really actually unlink) the FILE(s).
   -e, --extension=[EXT] When renaming a file, using EXT as the
                         extension. By default, EXT is trash.
   -t, --trashdir=[DIR]  When renaming a file, also create a symlink in
-                        DIR. By default, DIR is ~/.trash.
+                        DIR. By default, DIR is ~/.trash. A full path to
+                        the file from / will be created, and the symlink
+                        made inside that path.
   -n, --notrashdir      Do not symlink the file to the trash directory.
+  -o, --onlytrashdir    Instead of renaming the file, move it to the
+                        trash directory completley. 
 
 
 By default, trash does not remove directories.  Use the --recursive (-r
@@ -48,3 +56,108 @@ to recover all of its contents. The file will simply be renamed to
 be found in the trash folder (default is ~/.trash).
 EOF
 }
+
+# TODO: Add copyright function. 
+# TODO: Figure out copyright.
+# TODO: DAMNIT I NEED SOME VIM MACROS/TEMPLATES.
+version(){ 
+cat << EOF
+trash 1.0
+
+Copyright (C) 2012 William J. Bowman.
+
+Written by William J. Bowman <wjb@williamjbowman.com>
+EOF
+}
+
+# Parse arguments and input, side-effecting global variables used to
+# options. Returns a list of files to trash.
+parse_arguments(){
+  # TODO: Stub
+  echo "stub"
+}
+
+# Given a file, return it's absolute path.
+# TODO: Test
+get_absolute_path(){
+  pushd $(dirname "$1")
+  ABS_PATH="`pwd`"
+  popd
+  RETURN="$ABS_PATH"
+  return 0
+}
+
+# Given a file, return a new name in the form:
+# .<filename>.<timestamp>.<ext>
+# TODO: Test
+create_file_name(){
+  BASE=$(basename "$1")
+  TIME=$(date +%s)
+  RETURN="${BASE}${TIME}${EXT}"
+  BASE=""
+  TIME=""
+  return 0
+}
+
+# Given a file, create it's full path in DIR.
+# TODO: Test
+create_trash_path(){
+  verbose("Creating path for $1 in $DIR")
+  get_absolute_path("$1")
+  mkdir -p "$DIR/$RETURN" && verbose("Path created: $DIR/$RETURN")
+  RETURN=""
+  return 0
+}
+
+# Given a file, create it's symlink in DIR.
+# TODO: Test
+symlink_file(){
+  verbose("Creating symlink for $1 in $DIR")
+  get_absolute_path("$1")
+  ABS_PATH="$RETURN"
+  pushd "$DIR/$ABS_PATH"
+  create_file_name()
+  ln -s "$ABS_PATH/$RETURN" "$RETURN" && 
+    verbose("Link created at: $DIR/$ABS_PATH/$RETURN")
+  popd
+  RETURN=""
+  ABS_PATH=""
+  return 0
+}
+
+# Given a file, rename it in it's current directory.
+# TODO: Test
+rename_file(){
+  verbose("Renaming $1")
+  get_absolute_path("$1")
+  pushd "$RETURN"
+  create_file_name()
+  rename $(basename "$1") "$RETURN" && verbose("$1 renamed to $RETURN")
+  popd
+  RETURN=""
+  return 0
+}
+
+# Given a file, rename and symlink it, according to the specified
+# options.
+# TODO: Test
+# TODO: Make option aware.
+trash_file(){
+  verbose("Trashing $1")
+  rename_file("$1") && create_trash_path("$1") &&  symlink_file("$1") &&
+    verbose("$1 trashed!")
+  return 0
+}
+
+# Given a list of files, rename and symlink them according to the
+# specified options.
+# TODO: Test
+# TODO: make option aware.
+trash_files(){
+  for file in "${1[@]}"; do
+    trash_file("${file}")
+}
+
+# MAIN
+$FILES = parse_arguments()
+trash_files($FILES)
